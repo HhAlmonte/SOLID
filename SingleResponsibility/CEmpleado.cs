2389efec7f454de48ef2c6f350d0e921832d1cdf
Hector Almonte <hbalmontess272@gmail.com>
2022-09-24 20:47:33 -0300

    Agregar archivos de proyecto.

---
+++ SingleResponsibility/CEmpleado.cs
@@ -0,0 +1,42 @@
+﻿namespace SingleResponsibility
+{
+    public class CEmpleado
+    {
+        private string puesto;
+        private int edad;
+        public string Nombre { get; set; }
+        public double Salario { get; set; }
+
+        public CEmpleado(string nombre, string puesto, int edad, double salario)
+        {
+            Nombre = nombre;
+            this.puesto = puesto;
+            this.edad = edad;
+            Salario = salario;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1} {2} {3}",
+                                 Nombre, puesto, edad, Salario);
+        }
+
+
+        /* Incumpliendo el principio de responsabilidad única
+            * Esta clase tiene dos responsabilidades
+            * Lo correcto sería crear una clase independiente que se encargue de realizar estos procesos.
+            * En la clase de CHacienda se estaría viendo la manera correcta de como hacerlo.
+        */
+
+        /*public double CalcularImpuesto()
+        {
+            return salario * 0.35;
+        }
+
+        public void PagarImpuesto()
+        {
+            double imp = CalcularImpuesto();
+            Console.WriteLine("Se pagó {0} de impuestos, por parte de {1}", imp, nombre);
+        }*/
+    }
+}
---
+++ SingleResponsibility/CHacienda.cs
@@ -0,0 +1,19 @@
+﻿namespace SingleResponsibility
+{
+    public class CHacienda
+    {
+        // Se debe crear una clase especializada para la otra responsabilidad
+        // Ahora cada clase cuenta con una sola responsabilidad
+        
+        public static double CalcularImpuesto(CEmpleado cEmpleado)
+        {
+            return cEmpleado.Salario * 0.35;
+        }
+        
+        public static void PagarImpuesto(CEmpleado cEmpleado)
+        {
+            double imp = CalcularImpuesto(cEmpleado);
+            Console.WriteLine("Se pagó {0} en impuestos por parte de {1}", imp, cEmpleado.Nombre);
+        }
+    }
+}
